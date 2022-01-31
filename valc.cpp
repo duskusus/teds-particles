dc4177bd3fdf6330e482c29cefd0f11dc190ef57
Ted Josephson <tdj4@illinois.edu>
2022-01-30 23:17:05 -0600

    added vector addition

--- valc.cpp
+++ valc.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 #include <stdlib.h>
-
+#include <string>
+template <typename component_type>
+class vec2{
+	component_type x_component;
+	component_type y_component;
+	public:
+	vec2(component_type x, component_type y){
+		x_component = x;
+		y_component = y;
+	}
+	vec2 operator +(const vec2 &b){
+		return vec2(x_component + b.x_component, y_component + b.y_component);
+	}
+	vec2 operator -(const vec2 &b){
+		return vec2(x_component + b.x_component, y_component + b.y_component);
+	}
+	std::string text(){
+		return "<" + std::to_string(x_component) + ", " + std::to_string(y_component) + ">";
+	}
+};
 int main(int argc, char **argv) {
   unsigned int dimensions = 0;
   if (argv[1]) {
@@ -10,5 +29,8 @@ int main(int argc, char **argv) {
     return 0;
   }
   std::cout << "using " << dimensions << " dimensions\n";
+	vec2<float> v(1, 2);
+	vec2<float> w(3, 4);
+	std::cout << (v + w).text();
   return 0;
 }

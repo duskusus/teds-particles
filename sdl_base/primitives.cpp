0038f9c37f0a2944c86076c6f2c9155fa8551754
Ted Josephson <tdj4@illinois.edu>
2022-03-23 23:21:15 -0500

    triangle splitting and broken filling

--- sdl_base/primitives.cpp
+++ sdl_base/primitives.cpp
@@ -1,30 +1,41 @@
 #include "gUtil.h"
 #include <cmath>
 #include <iostream>
-
-inline vec2 greaterx(vec2 a, vec2 b) { return (a.intx() > b.intx()) ? a : b; }
-inline vec2 lesserx(vec2 a, vec2 b) { return (a.intx() < b.intx()) ? a : b; }
-inline vec2 greaterx(vec2 a, vec2 b, vec2 c) {
-  return greaterx(greaterx(a, b), c);
+void flip(vec2 *a, vec2 *b) {
+  vec2 temp = *a;
+  *a = *b;
+  *b = temp;
 }
-inline vec2 lesserx(vec2 a, vec2 b, vec2 c) {
-  return lesserx(lesserx(a, b), c);
+void sorty(vec2 *vs) {
+  // returns vertices sorted in ascending order by y value
+  if (vs[0].y > vs[1].y) {
+    flip(&vs[0], &vs[1]);
+  }
+  if (vs[1].y > vs[2].y) {
+    flip(&vs[1], &vs[2]);
+  }
+  if (vs[0].y > vs[1].y) {
+    flip(&vs[0], &vs[1]);
+  }
+  if (vs[0].y > vs[1].y || vs[1].y > vs[2].y) {
+    std::cout << "sorting failed" << std::endl;
+  }
 }
-inline vec2 midx(vec2 a, vec2 b, vec2 c) {
-  return lesserx(greaterx(a, b), greaterx(b, c), greaterx(a, c));
+void sortx(vec2 *vs) {
+  // returns vertices sorted in ascending order bx x value
+  if (vs[0].x > vs[1].x) {
+    flip(&vs[0], &vs[1]);
+  }
+  if (vs[1].x > vs[2].x) {
+    flip(&vs[1], &vs[2]);
+  }
+  if (vs[0].x > vs[1].x) {
+    flip(&vs[0], &vs[1]);
+  }
+  if (vs[0].x > vs[1].x || vs[1].x > vs[2].x) {
+    std::cout << "sorting failed" << std::endl;
+  }
 }
-inline vec2 greatery(vec2 a, vec2 b) { return (a.inty() > b.inty()) ? a : b; }
-inline vec2 lessery(vec2 a, vec2 b) { return (a.inty() < b.inty()) ? a : b; }
-inline vec2 greatery(vec2 a, vec2 b, vec2 c) {
-  return greatery(greatery(a, b), c);
-}
-inline vec2 lessery(vec2 a, vec2 b, vec2 c) {
-  return lessery(lessery(a, b), c);
-}
-inline vec2 midy(vec2 a, vec2 b, vec2 c) {
-  return lessery(greatery(a, b), greatery(b, c), greatery(a, c));
-}
-
 void Fbuffer::ntri(Color fill, vec2 a, vec2 b, vec2 c) {
 
   vec2 top;
@@ -33,24 +44,50 @@ void Fbuffer::ntri(Color fill, vec2 a, vec2 b, vec2 c) {
   // ftri(Color(0, 1, 0), a, b, c);
 
   float radius = 0.01; // for debugging circles
-
-  bool h_edge =
-      a.intx() == b.intx() || a.intx() == c.intx() || c.intx() == b.intx();
+  std::cout << "\n\n";
   bool v_edge =
+      a.intx() == b.intx() || a.intx() == c.intx() || b.intx() == c.intx();
+  bool h_edge =
       a.inty() == b.inty() || a.inty() == c.inty() || c.inty() == b.inty();
 
   if (h_edge && v_edge) {
     std::cout << "both\n";
     std::cout << a.about() << " " << b.about() << " " << c.about() << std::endl;
-    ftri(fill, a, b, c);
-  } else if (v_edge) {
+    // ftri(fill, a, b, c);
+    vec2 vx[] = {a, b, c};
+    sortx(vx);
+    vec2 vy[] = {a, b, c};
+    sorty(vy);
+    int dx = (vx[2] - vx[0]).intx();
+    int dy;
+    if(vx[0].intx() == vx[1].intx()) {
+      //we know vertical edge is on the left
+      vec2 extreme = (vx[0].inty() == vx[2].inty()) ? vx[1] : vx[0];
+      dy = (vx[2] - extreme).inty();
+    } else if(vx[1].intx() == vx[2].intx()) {
+      //we know vertical edge is on the right
+      vec2 extreme = (vx[1].inty() == vx[0].inty()) ? vx[2] : vx[1];
+      dy = (extreme - vx[0]).inty();
+    }
+    float slope = float(dy) / float(dx);
+    std::cout << "dx " << dx << " dy " << dy << " slope " << slope << std::endl;
+    for(int x = 0; x < dx; x ++) {
+      int h = vx[0].inty() + slope * x;
+      for(int y = 0; y * y < h * h; y += sign(dy)) {
+        set(x + vx[0].intx(), vx[0].y - y, fill);
+      }
+    }
+    tri(Color(1, 1, 1), a, b, c);
+  } else if (h_edge) {
 
-    std::cout << "vertical edge\n";
+    std::cout << "horizontal edge\n";
     // broken up into smaller triangles
-
-    top = greatery(a, b, c);
-    middle = midy(a, b, c);
-    bottom = lessery(a, b, c);
+    // sort by x
+    vec2 vertices[] = {a, b, c};
+    sortx(vertices);
+    top = vertices[2];
+    middle = vertices[1];
+    bottom = vertices[0];
 
     // top triangle vertices
     vec2 t_a, t_b, t_c;
@@ -60,16 +97,16 @@ void Fbuffer::ntri(Color fill, vec2 a, vec2 b, vec2 c) {
     // finding fourth, splitting point, intersection of y = mid.y and line from
     // top to bottom
     float slope = (bottom.y - top.y) / (bottom.x - top.x);
+    float k = top.y - slope * top.x;
     std::cout << "slope " << slope << std::endl;
-    float new_x =
-        (middle.y - bottom.y) * (bottom.x - top.x) / (bottom.y - top.y) +
-        bottom.x;
+    float new_y = slope * middle.x + k;
+    if (middle.x < top.x && middle.x > bottom.x)
 
-    // top triangle
+      // top triangle
 
-    t_a = top;
+      t_a = top;
     t_b = middle;
-    t_c = vec2(new_x, middle.y);
+    t_c = vec2(middle.x, new_y);
 
     std::cout << "top " << top.about() << "\nmid " << middle.about()
               << "\nbottom " << bottom.about() << "\nsplit " << t_c.about()
@@ -95,19 +132,17 @@ void Fbuffer::ntri(Color fill, vec2 a, vec2 b, vec2 c) {
 
       circle(Color(0, 1, 0), middle, radius);
     }
-  } else if (h_edge) {
+  } else if (v_edge) {
 
-    std::cout << "horizontal edge\n";
-
-    bottom = lesserx(a, b, c);
-    middle = midx(a, b, c);
-    top = greaterx(a, b, c);
+    std::cout << "vertical edge\n";
+    // sort by y
+    vec2 vertices[] = {a, b, c};
+    sorty(vertices);
+    bottom = vertices[0];
+    middle = vertices[1];
+    top = vertices[2];
 
     // broken up into smaller triangles
-
-    top = greaterx(a, b, c);
-    middle = midx(a, b, c);
-    bottom = lesserx(a, b, c);
 
     // right triangle vertices
     vec2 t_a, t_b, t_c;
@@ -155,12 +190,16 @@ void Fbuffer::ntri(Color fill, vec2 a, vec2 b, vec2 c) {
 
     set(middle, Color(0, 1, 0));
   } else {
-    std::cout << "neither\n";
-    // broken up into smaller triangles
 
-    top = greatery(a, b, c);
-    middle = midy(a, b, c);
-    bottom = lessery(a, b, c);
+    std::cout << "neither\n";
+
+    // broken up into smaller triangles
+    // sort by y
+    vec2 vertices[] = {a, b, c};
+    sorty(vertices);
+    top = vertices[2];
+    middle = vertices[1];
+    bottom = vertices[0];
 
     // top triangle vertices
     vec2 t_a, t_b, t_c;
@@ -170,10 +209,9 @@ void Fbuffer::ntri(Color fill, vec2 a, vec2 b, vec2 c) {
     // finding fourth, splitting point, intersection of y = mid.y and line from
     // top to bottom
     float slope = (bottom.y - top.y) / (bottom.x - top.x);
+    float k = top.y - slope * top.x;
     std::cout << "slope " << slope << std::endl;
-    float new_x =
-        (middle.y - bottom.y) * (bottom.x - top.x) / (bottom.y - top.y) +
-        bottom.x;
+    float new_x = (middle.y - k) / slope;
 
     // top triangle
 
@@ -191,11 +229,11 @@ void Fbuffer::ntri(Color fill, vec2 a, vec2 b, vec2 c) {
     b_b = middle;
     b_c = t_c;
 
-    //ntri(Color(1, 0, 0), t_a, t_b, t_c);
-    //ntri(Color(0, 0, 1), b_a, b_b, b_c);
+    // ntri(Color(1, 0, 0), t_a, t_b, t_c);
+    // ntri(Color(0, 0, 1), b_a, b_b, b_c);
 
-    ftri(Color(1, 0, 0), t_a, t_b, t_c);
-    ftri(Color(0, 1, 0), b_a, b_b, b_c);
+    ntri(Color(1, 0, 0), t_a, t_b, t_c);
+    ntri(Color(0, 1, 0), b_a, b_b, b_c);
 
     if (debugging_circles) {
       circle(Color(0, 0, 1), t_a, radius);
@@ -210,39 +248,39 @@ void Fbuffer::ntri(Color fill, vec2 a, vec2 b, vec2 c) {
     }
   }
 }
-  void Fbuffer::tri(Color col, vec2 a, vec2 b, vec2 c) {
-    iline(col, a, b);
-    iline(col, b, c);
-    iline(col, c, a);
-  }
-  void Fbuffer::circle(Color fill, vec2 center, float r) {
-    float w = float(_HEIGHT);
-    r *= w;
-    for (int x = r * -1; x < r; x++) {
-      int hh = r * r - x * x;
-      int h = sqrt(hh);
-      // std::cout << "x: " << x << " hh: " << hh << std::endl;
-      for (int y = 0 - h; y < 0 + h; y++) {
+void Fbuffer::tri(Color col, vec2 a, vec2 b, vec2 c) {
+  line(col, a, b);
+  line(col, b, c);
+  line(col, c, a);
+}
+void Fbuffer::circle(Color fill, vec2 center, float r) {
+  float w = float(_HEIGHT);
+  r *= w;
+  for (int x = r * -1; x < r; x++) {
+    int hh = r * r - x * x;
+    int h = sqrt(hh);
+    // std::cout << "x: " << x << " hh: " << hh << std::endl;
+    for (int y = 0 - h; y < 0 + h; y++) {
 
-        set(x + center.intx(), y + center.inty(), fill);
-      }
+      set(x + center.intx(), y + center.inty(), fill);
     }
   }
-  void Fbuffer::ftri(Color fill, vec2 a, vec2 b, vec2 c) {
-    float precision = 700;
-    vec2 d = b - a;
-    vec2 e = b - c;
-    d = d / d.len();
-    e = e / e.len();
-    d = d / precision;
-    e = e / precision;
+}
+void Fbuffer::ftri(Color fill, vec2 a, vec2 b, vec2 c) {
+  float precision = 200;
+  vec2 d = b - a;
+  vec2 e = b - c;
+  d = d / d.len();
+  e = e / e.len();
+  d = d / precision;
+  e = e / precision;
 
-    vec2 x = a;
-    vec2 y = c;
-    while ((x - b).len() > 1 / precision && (y - b).len() > 1 / precision) {
-      iline(fill, x, y);
-      x = x + d;
-      y = y + e;
-    }
-    tri(fill, a, b, c);
+  vec2 x = a;
+  vec2 y = c;
+  while ((x - b).len() > 1 / precision && (y - b).len() > 1 / precision) {
+    iline(fill, x, y);
+    x = x + d;
+    y = y + e;
+  }
+  tri(fill, a, b, c);
 }
